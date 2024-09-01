8cb06c12aeddc9fdd5845d0ae650131241293232
Uzairmemon10 <um889943@gmail.com>
2024-09-01 14:37:11 -0700

    by me

---
+++ script.js
@@ -0,0 +1,380 @@
+// alert("page not found")
+// confirm("quit from this file?"
+
+// let myname=("uzair")
+// fathername=("imran")
+// age=(20)
+// console.log(myname + fathername + age +" years old")
+
+// alert("it a smit class\nyou quit it")
+// let city=prompt("whats your city name" )
+// console.log(city)
+
+
+// let smit="saylani"
+// let a;
+
+
+// console.log(a)
+
+
+// let a=5
+// a++
+// ++a
+// ++a
+// a=5
+// a=a
+
+// console.log(a)
+
+
+
+// let b=5
+// b++
+
+// console.log(b++)
+// console.log(++b)
+
+
+
+// let a=3
+// let b=5
+// a=a+b
+// b=a+b
+// console.log(b)
+
+
+
+// console.log(2+2+2+2+2*0)
+
+// let a="uzair"
+// let b="Imran"
+// let c=20
+// let d="smit"
+// console.log(a)
+
+
+
+// let a=5
+// let b=2
+// console.log(a%b)
+
+// let number1 = Number(prompt("Write number 1"))
+// let number2 = Number(prompt("Write number 2"))
+// let operator = prompt("Write operator")
+
+// switch (operator){
+//     case "+":
+//     console.log(number1+number2);
+//     break;
+//     case "-":
+//     console.log(number1-number2);
+//     break;
+//     case "*":
+//     console.log(number1*number2);
+//     break;
+//     case "/":
+//     console.log(number1/number2);
+//     break;
+//     default:
+//     console.log("ERROR");
+// }
+
+// let table = Number(prompt("Any table"));
+// for(let i = 1; i <= 10; i++){
+//     console.log(table+ " x " + i + " = " +table*i)
+// }
+
+
+// let a=3
+// let b=5
+// console.log(a+b)
+
+
+// let a=3
+// let b=5
+// console.log(a-b)
+
+
+// let a=3
+// let b=5
+// console.log(a*b)
+
+//  let a=3
+//  let b=5
+//  console.log(a/b)
+
+// let a
+// document.write("value after declare is",typeof a)
+
+
+// let a=600
+// let b=5
+// console.log(a*b)
+// document.write("total cost to buy 5 tickets to a movie is", a*b)
+
+
+// let a=650
+// let b=3
+// console.log(a*b)
+
+
+// let c=100
+// let d=7
+// console.log(c*d)
+
+// let e=10
+// let f=10
+// console.log(e*f)
+
+
+// document.write("price of items is", a ,"<br>")
+// document.write("quantity of items", b,"<br>" )
+// document.write("price of item is", c,"<br>" )
+// document.write("quantity of items", d ,"<br>")
+// document.write("shipping fee", e*f,"<br>" )
+// document.write("total cost of your product is",a*b+c*d+e*f )
+
+
+
+// let a = 980
+// let b =804
+// document.write("total marks =",a,"<br>")
+// document.write("mark obtained =",b,"<br>")
+// document.write("percentage =",b*100/a,"<br>")
+
+// let a =104.80
+// let b = 10
+// console.log(a*b)
+
+// let c = 28
+// let d = 25
+// console.log(c*d)
+
+// document.write("total currency in pkr is =",a*b+c*d)
+
+// let usDoller = 104.80
+// let sr = 28
+// let value1 = usDoller * 10;
+// let value2 = sr * 25
+// console.log(value1+value2)
+
+
+// let a = 4
+// let b = a+5
+// let c = b*10
+// let d = c/2
+// console.log(d)
+
+
+// let string = "This is my javascript class"
+// let split = string.split(" ")
+// console.log(split)
+
+// let replace = string.replace("javascript","css")
+// console.log(replace)
+
+
+// let a=Number(prompt("enter your number"))
+// console.log(a)
+
+
+// let number=prompt("enter any num:");
+// if (number % 2===0){
+//  console.log("the number is even");
+// }else{("the number is odd");
+// }
+
+
+
+// let uzair=25
+// let abdulrehman=15
+// console.log(uzair<abdulrehman||uzair>abdulrehman&&uzair>abdulrehman&&uzair<abdulrehman)
+
+// let table=prompt("enter num" );
+// for(let i=1;i<=1000;i++){
+//     document.write(table+" x "+i+"  = "+i*table+"<br>");
+// }
+
+
+
+
+
+// if and else condition
+
+// let age= 25
+
+// if( age<26){
+//  console.log("can vote")
+// }
+// else(console.log ("cannot vote"))
+
+
+
+
+
+// let mode="dark"
+// let color;
+
+// if( mode=="dark"){
+//  color= "black"
+// }
+
+// if( mode=="light"){
+//     color= "white"
+//    }
+// console.log(color  )
+
+
+// let a=Number(prompt("enter number"))
+// if(a%2===0){
+//  console.log("even")
+// }
+// else(console.log("odd"))
+
+
+
+// ternery operators
+
+// let age =25
+
+// let result = age<18? "adult":"not adult"
+// console.log(result)
+
+// let a=Number (prompt( "enter number"))
+// if( a%5===0){
+// console.log(a, "is a multiple of 5")
+// }
+// else(console.log(a, "is not a multiple of 5"))
+
+
+// for loops
+
+// for(let i=1;i<=5;i++){
+// console.log(i)
+// }
+
+// let sum=0
+// for(let i=1;i<=5;i++ ){
+//  sum =sum+i
+//  console.log(sum)
+// }
+
+
+// let  a=1
+// while(a<=6){
+// console.log(a)
+// a++
+// }
+
+
+// let i=20
+// do {
+// console.log(i); 
+// i++   
+// }while (i<=25);
+    
+
+// let table=prompt("enter ANY num")
+// let ans = parseInt(prompt("write end number"))
+// let i=1
+// do{
+// console.log(table+" x "+i+" = " + i*table);
+// i++
+// }while(i<=ans);
+
+
+// for (let f=1 ; f<=5;f++){
+// console.log(f)    
+// }
+
+
+//  for of loops is basicallu used for string
+
+
+
+// let uzair = "apcd"
+// let size =0
+// for (let i of uzair){
+//    console.log(i)
+//   size++ 
+// }console.log(size)
+
+//  for in loop is basically used for object
+
+// let student ={
+// name :"uzair",
+// age :20,
+// area : "affanditoWn",
+// cast:"Memon",
+// }
+
+// student.count=30;
+// for (let key in student){
+//   console.log (key + student [key] )    
+// }
+// console.log(student)
+
+
+
+
+
+// let table =4
+// for ( i=1;i<=10;i++){
+// console.log(table+" x "+i+" = "+i*table )  
+// }
+
+
+// let table1 =4
+// let j=1
+// while (j<=10){
+// console.log(table1+" x "+j+" = "+j*table1)  
+// j++
+// }
+
+
+// let table1 =4
+// let j=1
+// do {
+//   console.log(table1+" x "+j+" = "+j*table1)
+// j++  
+// }while(j<=10);
+
+
+
+// let uza = "student"
+// let size = 0
+// for(let i of "student"){
+//  console.log(i)  
+// size++ 
+// }
+// console.log(size) 
+
+
+
+// let student={
+// car :"civic",
+// model : 2020,
+// varient : "eclass"
+// }
+
+// for (let key in student){
+// console.log(key + student[key])  
+// }
+
+
+
+// for (let table = 1 ;table<=100; table++ )
+// if(table%2!==0){
+//  console.log(table) 
+// }
+
+
+
+// let user = 25 
+// let num =prompt("guess the number")
+// while (user != num){
+//  num = prompt("you lost ther gaME .GUESS NUM AGAIN ")  
+// }
+// console.log("you won")
+
